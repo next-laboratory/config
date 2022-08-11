88808349fbb5eaf130833f43797930c0310503f4
chengyao <64066545+topyao@users.noreply.github.com>
2022-08-11 13:59:06 +0800

    Style: cs-fix

--- src/Contract/ConfigInterface.php
+++ src/Contract/ConfigInterface.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * @license  https://github.com/marxphp/max/blob/master/LICENSE
  */
 
-namespace Max\Config\Contracts;
+namespace Max\Config\Contract;
 
 interface ConfigInterface
 {
